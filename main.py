9af83c0d4a984dd18eef0762e17354e9afa30848
Loinen <dance_cucumber@mail.ru>
2020-10-28 18:59:12 +0300

    moments and distribution

--- main.py
+++ main.py
@@ -7,27 +7,23 @@ from scipy.stats import kde
 
 # Cчитываем файл
 # tb_data = pd.read_csv("global-summary-of-the-day-2020-10-23T07-37-38.csv", index_col=0)
-# print(tb_data)
-
 # Или берем только станцию, дату, температуру, индекс - дата
 tb_3cols = pd.read_csv("global-summary-of-the-day-2020-10-23T07-37-38.csv", index_col=1,
                        usecols=['STATION', 'DATE', 'TEMP', 'WDSP'])
 #print(tb_3cols)
 # кол-во уникальных станций
-#print(len(tb_3cols['STATION'].unique()))
 stations = tb_3cols['STATION'].unique().tolist()
 #print(stations)
-# Берем строку с выбранной датой
-#print("2010", stations[0])
-# print(tb_3cols.loc[['2010-01-01']])
-# print("2011")
-dates = pd.date_range(start='2010-01-01', end='2010-02-01', closed='left', freq='D')
-# print(dates.tolist())
+
+# tb_2cols = pd.read_csv("global-summary-of-the-day-2020-10-23T07-37-38.csv", index_col=0,
+#                        usecols=['DATE', 'TEMP'])
+# ax = tb_2cols.plot(figsize=(16, 5), title='Temperature')
+# ax.set_xlabel("Date")
+# ax.set_ylabel("Temperature")
+# plt.show()
 
 # Создаем датасет со средним значением для каждого месяца
-# сейчас тут скорее псевдокод
-cols = list(['Year','Jan', 'Feb', 'Mer', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
-#print(cols)
+cols = list(['Year','Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
 for station in stations:
     cols_values = list()
     month = tb_3cols.loc[tb_3cols['STATION'] == station]
@@ -42,30 +38,13 @@ for station in stations:
                 m1 = '{0}-{1}-01'.format(y, m)
                 m2 = '{0}-{1}-31'.format(y, m)
                 month_temp = month.loc[m1:m2]
-            #print(month_temp)
-            cols_year_values.append(np.mean(month_temp['TEMP'].tolist()))
+            cols_year_values.append((np.mean(month_temp['TEMP'].tolist()) - 32) / 1.8)
+            print(cols_year_values)
         cols_values.append([y]+cols_year_values)
 
-    # print(len(cols_values))
-    print(cols_values)
+    #print(cols_values)
 
     table_avg_temp = pd.DataFrame(cols_values, columns=cols)
-            # так создаем новую таблицу (это пример с документации)
-            # dfl = pd.DataFrame(np.random.randn(5, 4), columns = list('ABCD'),
-            #                    index=pd.date_range('20130101',periods=5))
-            # Тут добавляем строку в датафрейм (тоже пример)
-            # df = pd.DataFrame([[1, 'Bob', 8000],
-            #                    [2, 'Sally', 9000],
-            #                    [3, 'Scott', 20]], columns=['id', 'name', 'power level'])
-            # df.append(df.sum(axis=0), ignore_index=True)
-    # df_avg_temp1 = pd.DataFrame
-    # df_avg_temp = pd.concat([df_avg_temp1, df_avg_temp2], ignore_index=True)
-    # df_avg_temp.merge(df_avg_temp, left_on='DATE', right_on='DATE', suffixes=('_left', '_right'))
 
 table_avg_temp.to_csv('average_temp.csv')
-
-
-# tb_4cols = pd.read_csv("global-summary-of-the-day-2020-10-23T07-37-38.csv", index_col=1,
-#                       usecols=['STATION', 'DATE', 'TEMP', 'WDSP'])
-# print(tb_4cols)
-
+print(table_avg_temp)
--- one_dim_analysis.py
+++ one_dim_analysis.py
@@ -7,22 +7,63 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import re
 from scipy.stats import kde
+from scipy.stats.distributions import gamma
 
-cols = ['Jan', 'Feb', 'Mer', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+cols = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 tb_3cols = pd.read_csv("average_temp.csv", index_col=0,
                        usecols=['Year']+cols)
 
+precip = pd.read_table('nashville_precip.txt', index_col=0,na_values='NA',
+                       delim_whitespace=True)
 # histogram
-_ = tb_3cols.hist(grid=False)
+_ = tb_3cols.hist(grid=False, width=1.0) # sharex = True
+plt.margins(0.05, 0.05)
 plt.tight_layout()
 plt.show()
 
 # histogram and kernel estimation for one month
-yan_avg_temp = tb_3cols['Jan'].unique().tolist()
+# берем мат ожидание и дисперсию
+tb_3cols.fillna(value={'Jan': tb_3cols.Jan.mean()}, inplace=True)
+tb_3cols_mean = tb_3cols.mean()
+print(tb_3cols_mean)
+tb_3cols_var = tb_3cols.var()
+print(tb_3cols_var)
+# Посчитаем альфа и бета
+alpha_mom = tb_3cols_mean ** 2 / tb_3cols_var
+beta_mom = tb_3cols_var / tb_3cols_mean
+
+yan_avg_temp = tb_3cols['Jan'].tolist()
 plt.hist(yan_avg_temp, bins='auto', density=True)
 
 density = kde.gaussian_kde(yan_avg_temp)
-temp_grid = np.linspace(min(yan_avg_temp), max(yan_avg_temp), 10)
-plt.plot(yan_avg_temp, density(yan_avg_temp))
+# temp_grid = np.linspace(min(yan_avg_temp), max(yan_avg_temp), 10)
+# plt.plot(yan_avg_temp, density(yan_avg_temp))
+plt.plot(np.linspace(min(yan_avg_temp), max(yan_avg_temp)), gamma.pdf(np.linspace(min(yan_avg_temp), max(yan_avg_temp)), alpha_mom[0], beta_mom[0]))
 plt.show()
 
+# берем мат ожидание и дисперсию
+precip.fillna(value={'Oct': precip.Oct.mean()}, inplace=True)
+
+precip_mean = precip.mean()
+precip_var = precip.var()
+# Посчитаем альфа и бета
+palpha_mom = precip_mean ** 2 / precip_var
+pbeta_mom = precip_var / precip_mean
+yan_avg_temp = precip['Jan'].tolist()
+density = kde.gaussian_kde(yan_avg_temp)
+precip.Jan.hist(bins=20, density=True)
+
+plt.plot(np.linspace(0, 10), gamma.pdf(np.linspace(0, 10), palpha_mom[0], pbeta_mom[0]))
+plt.show()
+
+tb_3cols.fillna(value={'Jan': tb_3cols.Jan.mean()}, inplace=True)
+tb_3cols_mean = tb_3cols.mean()
+tb_3cols_var = tb_3cols.var()
+# Посчитаем альфа и бета
+alpha_mom = tb_3cols_mean ** 2 / tb_3cols_var
+beta_mom = tb_3cols_var / tb_3cols_mean
+yan_avg_temp = tb_3cols['Jan'].tolist()
+density = kde.gaussian_kde(yan_avg_temp)
+tb_3cols.Jan.hist(bins='auto', density=True)
+plt.plot(np.linspace(min(yan_avg_temp), max(yan_avg_temp)), gamma.pdf(np.linspace(min(yan_avg_temp), max(yan_avg_temp)), alpha_mom[0], beta_mom[0]))
+plt.show()
