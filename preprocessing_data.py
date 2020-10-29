dda96a0fdd4540481ce1ca40cd60c7914223b5e9
Tatiana Tikhonova <ttixonova98@gmail.com>
2020-10-29 19:46:27 +0300

    Hello

--- preprocessing_data.py
+++ preprocessing_data.py
@@ -60,6 +60,7 @@ def main():
     df_from_each_file = (pd.read_csv(f) for f in all_files)
     concatenated_df = pd.concat(df_from_each_file, ignore_index=True)
     concatenated_df.to_csv("data/average_temp.csv")
+    print("Hello")
 
 
 if __name__ == '__main__':
