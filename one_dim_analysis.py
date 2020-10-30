d4faf1b751124381af0fbb35ce3316d8484d8ba5
Loinen <dance_cucumber@mail.ru>
2020-10-30 19:40:26 +0300

    pictures saves now and little changes in func

--- one_dim_analysis.py
+++ one_dim_analysis.py
@@ -50,12 +50,14 @@ lin = np.linspace(min(yan_avg_temp), max(yan_avg_temp))
 # tb_3cols.Oct.hist(bins='auto', density=True)
 # plt.plot(yan_avg_temp, density(yan_avg_temp))
 # plt.show()
+fig, ax = plt.subplots()
+plt.title("apr hist")
 plt.plot(lin, gamma.pdf(lin, alpha_mom[0], beta_mom[0]))
 plt.show()
-plt.savefig('apr histogram')
+fig.savefig('apr histogram')
 
+fig, ax = plt.subplots()
 axs = tb_3cols.hist(bins=15, density=True, figsize=(12, 8), sharex=True, sharey=True, grid=False)
-
 for ax in axs.ravel():
     m = ax.get_title()
     x = np.linspace(*ax.get_xlim())
@@ -63,10 +65,11 @@ for ax in axs.ravel():
     label = 'alpha = {0:.2f}\nbeta = {1:.2f}'.format(alpha_mom[m], beta_mom[m])
     ax.annotate(label, xy=(10, 0.2))
 plt.show()
-plt.savefig('gamma histogram')
+fig.savefig('gamma histogram')
 
 
 # пуассон
+plt.title("Пуассон")
 y = np.random.poisson(5, size=100)
 plt.hist(y, bins=12)
 plt.xlabel('y')
@@ -86,6 +89,7 @@ plt.show()
 
 lam = 5
 xvals = np.arange(15)
+plt.title("Правдоподобие?")
 plt.bar(xvals, [poisson_like(x, lam) for x in xvals])
 plt.xlabel('x')
 plt.ylabel('Pr(X|($\lambda$|=5')
@@ -107,10 +111,13 @@ tanline = lambda x: -0.858 + 0.626*x
 plt.plot(xvals, tanline(xvals), 'r--')
 # point at step n+1
 xprime = 0.858/0.626
+fig, ax = plt.subplots()
+
+plt.title("Метод Ньютона-Рапсона")
 plt.plot([xprime, xprime], [tanline(xprime), func(xprime)], 'k:')
 plt.text(xprime+.1, -.2, '$x_{n+1}$', fontsize=16)
 plt.show()
-plt.savefig('Newton1')
+fig.savefig('Newton1')
 
 
 dlgamma = lambda m, log_mean, mean_log: np.log(m) - psi(m) - log_mean + mean_log
@@ -142,25 +149,30 @@ x = np.linspace(0, 10, 100)
 s = 0.4
 # Calculate the kernels
 kernels = np.transpose([norm.pdf(x, yi, s) for yi in y])
+fig, ax = plt.subplots()
+
+plt.title("Calculate the kernels")
 plt.plot(x, kernels, 'k:')
 plt.plot(x, kernels.sum(1))
 plt.plot(y, np.zeros(len(y)), 'ro', ms=10)
 plt.show()
-plt.savefig('kernels')
+fig.savefig('kernels')
 
+fig, ax = plt.subplots()
 x1 = np.random.normal(0, 3, 50)
 x2 = np.random.normal(4, 1, 50)
 x = np.r_[x1, x2]
 plt.hist(x, bins=8)
 plt.show()
-plt.savefig('kernels2')
+fig.savefig('kernels2')
 
+fig, ax = plt.subplots()
 density = kde.gaussian_kde(x)
 xgrid = np.linspace(x.min(), x.max(), 100)
 plt.hist(x, bins=8, density=True)
 plt.plot(xgrid, density(xgrid), 'r-')
 plt.show()
-plt.savefig('kernels3')
+fig.savefig('kernels3')
 
 
 # по второму примеру - доверительные интервалы, выборочные статистики
@@ -173,10 +185,13 @@ plt.figure(figsize=(10, 8))
 # указываем X и Y
 print(tb_3cols['TEMP'])
 print(tb_3cols['DATE'])
+plt.title("Дата и температура по станции 26063099999")
 plt.scatter(tb_3cols['DATE'], tb_3cols['TEMP'])
 plt.xticks(rotation=45)
-plt.xlabel(u'Номер клиента', fontsize=20)
-plt.ylabel(u'Средняя транзакция', fontsize=20)
+plt.xlabel(u'Дата', fontsize=20)
+plt.ylabel(u'Температура (Фаренгейты)', fontsize=20)
+plt.legend()
+plt.show()
 
 # Вычисление выборочного среднего, дисперсии, СКО, медианы
 mean = tb_3cols['TEMP'].mean()
@@ -187,24 +202,19 @@ median = tb_3cols['TEMP'].median()
 # Вычисление усеченного среднего, с усечением 10% наибольших и наименьших значений
 trimmed_mean = sp.stats.trim_mean(tb_3cols['TEMP'], proportiontocut=0.1)
 
-
 # median absolute deviation
 def mad(df):
     # параметр для логнормального распределения
     sigma = 1.2
     k = sp.stats.lognorm.ppf(3 / 4., s=sigma)
     median = df.median()
-
     return k * np.median(np.fabs(df - median))
-
 
 # Вычисление MAD-характеристики (Median Absolute Deviation)
 mad_value = mad(tb_3cols['TEMP'])
 
 print(f'Средний размер транзакции: среднее = {int(mean)}, дисперсия = {int(var)}, СКО = {int(std)},\n'
       f'медиана = {int(median)}, усеченное среднее {int(trimmed_mean)}, MAD = {int(mad_value)}')
-
-# %%
 
 # Расчет 95% доверительного интервала для выборочного среднего
 norm_q95 = sp.stats.norm.ppf(0.95)
@@ -219,31 +229,34 @@ var_conf_right = var * (len(tb_3cols) - 1) / chi2_q95_right
 std_conf_left = np.sqrt(var_conf_left)
 std_conf_right = np.sqrt(var_conf_right)
 
-# Вывод полученных значений в консоль
+# Вывод полученных значений
 print("Выборочное среднее: %0.3f +/- %0.3f" % (mean, mean_conf))
 print("95%% Доверительный интервал выборочной дисперсии : (%0.3f; %0.3f)"
       % (var_conf_left, var_conf_right))
 print("95%% Доверительный интервал выборочного СКО: (%0.3f; %0.3f)"
       % (std_conf_left, std_conf_right))
 
-# %%
-
 # Построение гистограммы и ядерной оценки плотности
 plt.figure(figsize=(10, 8))
+plt.title("Построение гистограммы и ядерной оценки плотности")
 
 kernel = sp.stats.gaussian_kde(tb_3cols['TEMP'])
-
 min_amount, max_amount = tb_3cols['TEMP'].min(), tb_3cols['TEMP'].max()
 x = np.linspace(min_amount, max_amount, len(tb_3cols))
 kde_values = kernel(x)
 
-sns.distplot(tb_3cols['TEMP'], kde=False, norm_hist=True, label=f'Средний размер транзакции в рублях')
+den = tb_3cols['TEMP'].tolist()
+density = kde.gaussian_kde(sorted(den))
+sns.displot(tb_3cols['TEMP'], kde=False, norm_hist=True, label=f'Средний размер транзакции в рублях')
 plt.plot(x, kde_values)
+plt.show()
 
+fig, ax = plt.subplots()
+plt.title("Построение гистограммы и ядерной оценки плотности")
 plt.ylabel('p')
 plt.xlabel('Средний размер транзакции')
 # Отображаем значения по оси абсцисс только в интервале [0, 10000]
 plt.xlim(0, 10e4)
 plt.legend()
 plt.show()
-
+fig.savefig('ядерной оценки плотности')
