eb97060177338f5c35e3486a86c79fa3719670b9
Loinen <dance_cucumber@mail.ru>
2020-10-31 18:47:47 +0300

    nothing important, just comments

--- one_dim_analysis.py
+++ one_dim_analysis.py
@@ -1,7 +1,6 @@
 """
 One-dimensional data analysis. Nonparametric estimation
 """
-import csv
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -15,7 +14,7 @@ from scipy.special import psi, polygamma
 from scipy.stats import kde
 import seaborn as sns
 
-
+# Берем одну станцию, со средней температурой для каждого месяца в году
 cols = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 tb_3cols = pd.read_csv("data/average_temp_26063099999.csv", index_col=0,
                        usecols=['Year']+cols)
@@ -23,8 +22,9 @@ tb_3cols = pd.read_csv("data/average_temp_26063099999.csv", index_col=0,
 print("Average temperatures in each month")
 print(tb_3cols)
 
-# histogram
+# непараметрические оценки - 1 - histogram
 _ = tb_3cols.hist(grid=False)  # sharex=True, width=0.5
+plt.title("histogram")
 plt.margins(0.05, 0.05)
 plt.tight_layout()
 plt.show()
@@ -39,6 +39,7 @@ tb_3cols_var = tb_3cols.var()
 print("Temperature variances for each month")
 print(tb_3cols_var)
 
+# непараметрические оценки - 2 - ядерные оценки
 # Посчитаем альфа и бета
 alpha_mom = tb_3cols_mean ** 2 / tb_3cols_var
 beta_mom = tb_3cols_var / tb_3cols_mean
@@ -67,7 +68,6 @@ for ax in axs.ravel():
 plt.show()
 fig.savefig('gamma histogram')
 
-
 # пуассон
 plt.title("Пуассон")
 y = np.random.poisson(5, size=100)
@@ -80,6 +80,7 @@ poisson_like = lambda x, lam: np.exp(-lam) * (lam**x) / (np.arange(x)+1).prod()
 
 lambda5 = np.linspace(0, 15)
 x = 5
+plt.title("Пуассон")
 plt.plot(lambda5, [poisson_like(x, l) for l in lambda5])
 plt.xlabel('$\lambda$')
 plt.ylabel('L($\lambda$|x={0}'.format(x))
@@ -96,29 +97,24 @@ plt.ylabel('Pr(X|($\lambda$|=5')
 plt.show()
 
 # Метод Ньютона-Рапсона
-
-func = lambda x: 3./(1 + 400*np.exp(-2*x)) - 1
-xvals = np.linspace(0, 6)
-plt.plot(xvals, func(xvals))
-plt.text(5.3, 2.1, '$f(x)$', fontsize=16)
-# zero line
-plt.plot([0,6], [0,0], 'k-')
-# value at step n
-plt.plot([4,4], [0,func(4)], 'k:')
-plt.text(4, -.2, '$x_n$', fontsize=16)
-# tangent line
-tanline = lambda x: -0.858 + 0.626*x
-plt.plot(xvals, tanline(xvals), 'r--')
-# point at step n+1
-xprime = 0.858/0.626
-fig, ax = plt.subplots()
-
-plt.title("Метод Ньютона-Рапсона")
-plt.plot([xprime, xprime], [tanline(xprime), func(xprime)], 'k:')
-plt.text(xprime+.1, -.2, '$x_{n+1}$', fontsize=16)
-plt.show()
-fig.savefig('Newton1')
-
+# func = lambda x: 3./(1 + 400*np.exp(-2*x)) - 1
+# xvals = np.linspace(0, 6)
+# plt.plot(xvals, func(xvals))
+# plt.text(5.3, 2.1, '$f(x)$', fontsize=16)
+# # zero line
+# plt.plot([0,6], [0,0], 'k-')
+# # value at step n
+# plt.plot([4,4], [0,func(4)], 'k:')
+# plt.text(4, -.2, '$x_n$', fontsize=16)
+# # tangent line
+# tanline = lambda x: -0.858 + 0.626*x
+# plt.plot(xvals, tanline(xvals), 'r--')
+# # point at step n+1
+# xprime = 0.858/0.626
+# plt.title("Метод Ньютона-Рапсона")
+# plt.plot([xprime, xprime], [tanline(xprime), func(xprime)], 'k:')
+# plt.text(xprime+.1, -.2, '$x_{n+1}$', fontsize=16)
+# plt.show()
 
 dlgamma = lambda m, log_mean, mean_log: np.log(m) - psi(m) - log_mean + mean_log
 dl2gamma = lambda m, *args: 1./m - polygamma(1, m)
@@ -128,12 +124,15 @@ mean_log = tb_3cols.apply(np.log).mean()
 
 print(dlgamma, dl2gamma)
 print(log_mean, mean_log)
+
 # alpha_mle = newton(dlgamma, 2, dl2gamma, args=(log_mean[-2], mean_log[-2]))
 # beta_mle = alpha_mle/tb_3cols.mean()[-2]
 #
 # dec = tb_3cols.Nov
 # dec.hist(bins=10, grid=False, density=True)
+# fig, ax = plt.subplots()
 # x = np.linspace(0, dec.max())
+# plt.title("Метод Ньютона-Рапсона")
 # plt.plot(x, gamma.pdf(x, alpha_mom[-2], beta_mom[-2]), 'm-')
 # plt.plot(x, gamma.pdf(x, alpha_mle, beta_mle), 'r--')
 # plt.show()
@@ -202,6 +201,7 @@ median = tb_3cols['TEMP'].median()
 # Вычисление усеченного среднего, с усечением 10% наибольших и наименьших значений
 trimmed_mean = sp.stats.trim_mean(tb_3cols['TEMP'], proportiontocut=0.1)
 
+
 # median absolute deviation
 def mad(df):
     # параметр для логнормального распределения
@@ -210,9 +210,10 @@ def mad(df):
     median = df.median()
     return k * np.median(np.fabs(df - median))
 
+
 # Вычисление MAD-характеристики (Median Absolute Deviation)
 mad_value = mad(tb_3cols['TEMP'])
-
+# выборочное среднее, СКО, медиана, доверительные интервалы
 print(f'Средняя температура Фаренгейт (за все годы, что не очень полезно): среднее = {int(mean)}, дисперсия = {int(var)}, СКО = {int(std)},\n'
       f'медиана = {int(median)}, усеченное среднее {int(trimmed_mean)}, MAD = {int(mad_value)}')
 
@@ -247,7 +248,7 @@ kde_values = kernel(x)
 
 den = tb_3cols['TEMP'].tolist()
 density = kde.gaussian_kde(sorted(den))
-#norm_hist=True,
+# norm_hist=True,
 sns.displot(tb_3cols['TEMP'], kde=False,  label=f'Средняя температура')
 plt.plot(x, kde_values)
 plt.show()
@@ -273,14 +274,17 @@ plt.legend()
 tb_2cols.hist()
 plt.show()
 
-plt.title(" График распределения с использованием функции сглаживания")
+fig = plt.figure()
+plt.title("График распределения с использованием функции сглаживания")
 plt.xlabel(u'Дата', fontsize=20)
 plt.ylabel(u'Температура (Фаренгейты)', fontsize=20)
 plt.legend()
 tb_2cols.plot.kde()
 plt.show()
+fig.savefig('График распределения с использованием функции сглаживания.png')
 
-# ящик
+# ящик с усами
+fig, ax = plt.subplots()
 srez = tb_2cols.loc['2000-01-01':'2000-12-31']
 srez.index = pd.to_datetime(srez.index)
 print(srez)
@@ -291,4 +295,6 @@ month = pd.DataFrame(month)
 month.columns = range(1, 13)
 month.boxplot()
 plt.show()
+fig.savefig('Ящик с усами.png')
+
 
