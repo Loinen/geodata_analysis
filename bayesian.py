be6abc48a3b9e1e44bcbcc13869c557fa49d1b85
Loinen <dance_cucumber@mail.ru>
2020-12-23 18:55:09 +0300

    more beautiful code...

--- bayesian.py
+++ bayesian.py
@@ -51,7 +51,16 @@ def accuracy_params_restoration(bn: BayesianModel, data: pd.DataFrame):
     return result
 
 
-def sampling(bn: DAG, data: pd.DataFrame, n: int = 100):
+def sampling(bn: DAG, data: pd.DataFrame, n):
+    G = nx.DiGraph()
+    G.add_edges_from(bn.edges())
+    pos = nx.layout.circular_layout(G)
+    nx.draw(G, pos, with_labels=True, font_weight='bold')
+    plt.show()
+
+    accuracy = accuracy_params_restoration(BayesianModel(bn.edges()), data)
+    print(accuracy)
+
     bn_new = BayesianModel(bn.edges())
     bn_new.fit(data)
     sampler = BayesianModelSampling(bn_new)
@@ -60,9 +69,6 @@ def sampling(bn: DAG, data: pd.DataFrame, n: int = 100):
 
 
 if __name__ == "__main__":
-    # data = pd.read_csv("data/data_spb.csv",
-    #                    usecols=['STATION', 'TEMP', 'WDSP', 'SLP', 'GUST', 'SNDP'], index_col=0)
-
     data = pd.read_csv("data/data_spb.csv", usecols=['STATION', 'SLP', 'DEWP', 'MAX',
                                                      'MIN', 'TEMP', 'WDSP'], index_col=0)
     data = data.loc[26063099999]
@@ -82,69 +88,19 @@ if __name__ == "__main__":
     sns.heatmap(corr, annot=True, fmt='.1f', cmap='Blues')
     plt.show()
 
-    pd.options.display.max_rows = 500
-    pd.options.display.max_columns = 500
-    print(data.head(201))
-    print(len(data))
-    # ограничиваем число данных, выбираем параметры для ручного построения байесовской сети
+    # ограничиваем число данных, выбираем параметры для ручного добавления ребер байесовской сети
     data = data[1:2000]
     data2 = data[['DEWP', 'SLP', 'TEMP', 'WDSP']]
 
     bins = 4
-    # на случай, если бинов недостаточно
-    # while True:
-    #     try:
-    #
-    #         break
-    #
-    #     except KeyError:
-    #         print(bins)
-    #         bins = bins+1
-    #         continue
     transformed_data = copy(data)
     transformed_data2 = copy(data2)
 
     est = KBinsDiscretizer(n_bins=bins, encode='ordinal', strategy='kmeans')
-    est2 = KBinsDiscretizer(n_bins=bins, encode='ordinal', strategy='kmeans')
-
     data_discrete = est.fit_transform(transformed_data.values[:, 0:6])
     transformed_data[['DEWP', 'MAX', 'MIN', 'SLP', 'TEMP', 'WDSP']] = data_discrete
-    hc = HillClimbSearch(transformed_data, scoring_method=BDeuScore(transformed_data))
-    best_model = hc.estimate()
-
-    G_K2 = nx.DiGraph()
-    G_K2.add_edges_from(best_model.edges())
-    pos = nx.layout.circular_layout(G_K2)
-    nx.draw(G_K2, pos, with_labels=True, font_weight='bold')
-    plt.show()
-
-    accuracy = accuracy_params_restoration(BayesianModel(best_model.edges()), transformed_data)
-    print(accuracy)
-
-    # ручное
-    data_discrete2 = est2.fit_transform(data2.values[:, 0:4])
-    transformed_data2[['DEWP', 'SLP', 'TEMP', 'WDSP']] = data_discrete2
-    hc = HillClimbSearch(transformed_data2, scoring_method=BDeuScore(transformed_data2))
-    best_model2 = hc.estimate()
-
-    # создаем узлы для байесовской сети
-    bm = BayesianModel([('TEMP', 'SLP'), ('WDSP', 'SLP'), ('DEWP', 'TEMP')])
-
-    G_K2 = nx.DiGraph()
-    G_K2.add_edges_from(bm.edges())
-    pos = nx.layout.circular_layout(G_K2)
-    nx.draw(G_K2, pos, with_labels=True, font_weight='bold')
-    plt.show()
-    print(bm.get_cpds())
-    print(transformed_data2)
-
-    accuracy2 = accuracy_params_restoration(BayesianModel(best_model2.edges()), transformed_data2)
-    ##
     hc_BicScore = HillClimbSearch(transformed_data, scoring_method=BicScore(transformed_data))
     best_model_BicScore = hc_BicScore.estimate()
-
-    print("shape", transformed_data.shape)
-
     sample_Bic = sampling(best_model_BicScore, transformed_data, len(data))
     sample_Bic[['DEWP', 'MAX', 'MIN', 'SLP',  'TEMP', 'WDSP']] = est.inverse_transform(sample_Bic[
                ['DEWP', 'MAX', 'MIN', 'SLP',  'TEMP', 'WDSP']].values)
@@ -154,9 +110,14 @@ if __name__ == "__main__":
     draw_comparative_hist('TEMP', transformed_data, sample_Bic)
     draw_comparative_hist('WDSP', transformed_data, sample_Bic)
 
-    hc_BicScore = HillClimbSearch(transformed_data2, scoring_method=BicScore(transformed_data2))
-    best_model_BicScore = hc_BicScore.estimate()
-    sample_Bic2 = sampling(best_model_BicScore, transformed_data2, len(data2))
+    # ручное
+    est2 = KBinsDiscretizer(n_bins=bins, encode='ordinal', strategy='kmeans')
+    data_discrete2 = est2.fit_transform(data2.values[:, 0:4])
+    transformed_data2[['DEWP', 'SLP', 'TEMP', 'WDSP']] = data_discrete2
+    hc_BicScore2 = HillClimbSearch(transformed_data2, scoring_method=BicScore(transformed_data2))
+    best_model_BicScore2 = hc_BicScore2.estimate(white_list=[('TEMP', 'SLP'), ('WDSP', 'SLP'), ('DEWP', 'TEMP')])
+
+    sample_Bic2 = sampling(best_model_BicScore2, transformed_data2, len(data2))
     sample_Bic2[['DEWP', 'SLP', 'TEMP', 'WDSP']] = est2.inverse_transform(sample_Bic2[
                 ['DEWP', 'SLP', 'TEMP', 'WDSP']].values)
 
@@ -166,26 +127,25 @@ if __name__ == "__main__":
     draw_comparative_hist('WDSP', transformed_data2, sample_Bic2)
 
     sns.distplot(data['WDSP'], label='Original data')
-    sns.distplot(sample_Bic['WDSP'], label='Generated data dist')
+    sns.distplot(sample_Bic['WDSP'], label='Generated data')
+    sns.distplot(sample_Bic2['WDSP'], label='Generated data dist')
     plt.legend()
     plt.show()
 
     sns.distplot(data['TEMP'], label='Original data')
     sns.distplot(sample_Bic['TEMP'], label='Generated data')
+    sns.distplot(sample_Bic2['TEMP'], label='Generated data dist')
     plt.legend()
     plt.show()
 
     sns.distplot(data['SLP'], label='Original data')
     sns.distplot(sample_Bic['SLP'], label='Generated data')
+    sns.distplot(sample_Bic2['SLP'], label='Generated data dist')
     plt.legend()
     plt.show()
 
     sns.distplot(data['DEWP'], label='Original data')
     sns.distplot(sample_Bic['DEWP'], label='Generated data')
+    sns.distplot(sample_Bic2['DEWP'], label='Generated data dist')
     plt.legend()
     plt.show()
-
-    print(accuracy)
-    print(accuracy2)
-    print("bins", bins)
-    print(sample_Bic['SLP'])
